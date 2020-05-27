b32ed0ed247d42b4397f2f4950d0aff36f609d5c
Joao Neto <neto.com@msn.com>
2020-05-27 16:59:52 -0300

    Processamento do menu

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -59,6 +59,49 @@ public class Programa {
 
 		}
 
+		// MENU
+
+		int opcaoMenu = 0;
+		while (opcaoMenu != 4) {
+
+			System.out.println();
+			System.out.println("1 - Mostrar valor da entrada");
+			System.out.println("2 - Mostrar o valor financiado");
+			System.out.println("3 - Mostrar valor de cada prestação");
+			System.out.println("4 - Sair");
+			System.out.print("Digite uma opção: ");
+
+			opcaoMenu = sc.nextInt();
+
+			if (opcaoMenu == 1) {
+
+				System.out.println();
+				System.out.printf("ENTRADA = R$ %.2f%n", valorEntrada);
+
+			} else if (opcaoMenu == 2) {
+
+				System.out.println();
+				System.out.printf("VALOR FINANCIADO = R$ %.2f%n", valorFinanciado);
+
+			} else if (opcaoMenu == 3) {
+
+				System.out.println();
+				System.out.printf("VALOR DE CADA PRESTAÇÃO = R$ %.2f%n", prestacao);
+
+			} else if (opcaoMenu == 4) {
+
+				System.out.println();
+				System.out.println("FIM DO PROGRAMA!");
+
+			} else {
+
+				System.out.println();
+				System.out.println("OPÇÃO INVÁLIDA");
+
+			}
+
+		}
+
 	}
 
 }
